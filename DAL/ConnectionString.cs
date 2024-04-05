1400dc7269051916773b4f5bae1cbbce37511f19
DarkPheonix29 <kealanthal29@gmail.com>
2024-04-05 11:49:43 +0200

    Mysqlconnection added

---
+++ DAL/ConnectionString.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class ConnectionString
+    {
+        public static MySqlConnection GetConnection()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            return new MySqlConnection(configuration.GetConnectionString("MysqlConnection"));
+        }
+    }
+}
--- GT-FindWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GT_FindWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
