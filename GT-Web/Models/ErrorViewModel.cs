7a7e329b0504c4edf42afc58a5b71a7c8ffa9f92
DarkPheonix29 <kealanthal29@gmail.com>
2024-04-15 09:41:01 +0200

    DBconnection merge

---
+++ GT-Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GT_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
